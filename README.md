da6ba02000f4ff37281a76c2fea240f0639e6657
miiizen <jamesmmizen@gmail.com>
2017-10-19 18:07:00 +0100

    Initail commit
    Add readme.md and first implementation

---
+++ README.md
@@ -0,0 +1,10 @@
+# Data Structures
+This is a repositry containing example implementations of data structures, specifically relevant to the [AQA A-Level Computing](https://en.wikibooks.org/wiki/A-level_Computing/AQA) [specification](http://www.aqa.org.uk/subjects/computer-science-and-it/as-and-a-level/computer-science-7516-7517).
+There are currently implementations in:
+ - Python
+ - C#
+ - C++
+so far, however if you would like to add another language, by all means submit a pull request.
+
+## Pull requests
+If you would like to make a change or add a language, just submit a pull request with a summary of your changes.  Additionally, if you feel more could be added in terms of documentation, for example explanitory .md files for each structure you're more than welcome to do this.
---
+++ Stack/Python/Stack.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+
+class Stack:
+    """ An implementation of a stack """
+    MAX_SIZE = 8
+    # Initialise list
+    stack_ = [""] * MAX_SIZE
+    top = 0
+ 
+    def __init__(self, items=None):
+        """ Init, take optional argument items """
+        # Check the user has specified a list of items
+        if(items != None):
+            # Check the list of items is not too long
+            if(len(items) > self.MAX_SIZE):
+                raise IndexError("The list has more than 64 items in it!")
+            else:
+                # Put the items in the stack
+                for i in range(0, len(items)):
+                    self.stack_[i] = items[i]
+                # The next available slot will be the same as the length
+                self.top = len(items)
+        else:
+            self.top = 0
+
+    def push(self, item):
+        """ Push a value onto the stack """
+        if(self.isFull()):
+            raise IndexError("The stack is full")
+        else:
+            self.stack_[self.top] = item
+            self.top += 1
+    
+    def pop(self):
+        """ Pop an item onto the stack """
+        item = self.stack_[self.top - 1]
+        self.top -= 1
+        return item
+
+    def peek(self):
+        """ Peek top item without popping it """
+        return self.stack_[self.top - 1]
+
+    def clear(self):
+        """ Clear the stack """
+        top = 0
+
+    def __len__(self):
+        """ Hook method for returning size of stack using len(stack) """
+        return self.top
+    
+    def isEmpty(self):
+        """ Bool, returns true or false based on whether the stack is empty or not """
+        if(self.top == 0):
+            return True
+        else:
+            return False
+
+    def isFull(self):
+        """ Bool, returns true or false based on whether the stack is full or not """
+        if(self.top == self.MAX_SIZE):
+            return True
+        else:
+            return False
+
+""" Main - Tests """
+if(__name__ == "__main__"):
+    # Init and add to stack
+    s = Stack(["item 1", "item 2", "item 3", "item 4"])
+    s.push("item 5")
+    s.push("item 6")
+    s.push("item 7")
+    s.push("item 8")
+    # Check if full (yes)
+    print("Is full: " + str(s.isFull()))
+    # peek then pop
+    print("Peek: " + s.peek())
+    print("Pop: " + s.pop())
+    print("Len: " + str(len(s)))
